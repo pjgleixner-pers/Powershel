27f2b430bbfb982d6eeda32b36e8ad8c009c82f9
Pieter Gleixner <pjgleixner@msn.com>
2025-07-07 15:10:27 -0400

    Update HashTable.ps1

---
+++ Learning101/HashTable.ps1
@@ -0,0 +1,16 @@
+$Fellowship = @{ "Wizzard" = "Gandalf" ; "Hobbit" = "Frodo" ; "Elf" = "Legolas"; "EvilWizzard" = "Sauron"}
+#$Fellowship
+
+#Add
+$Fellowship.Add("Dwarf","Gimli")
+#$Fellowship
+
+#edit
+$Fellowship.Set_Item("Dwarf","Bilbo")
+#$Fellowship
+
+#$Fellowship.Hobbit
+
+#Remove
+$Fellowship.Remove("EvilWizzard")
+$Fellowship
