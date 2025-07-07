3e7d38b15049126b00d7fc2d349df1fa1eda2ff6
Pieter Gleixner <pjgleixner@msn.com>
2025-07-07 16:38:05 -0400

    Update WhileLoop.ps1

--- Learning101/WhileLoop.ps1
+++ Learning101/WhileLoop.ps1
@@ -1,1 +1,17 @@
-$Xmen =@('Wolvorine')
+$Xmen = @('Wolvorine', 'Cyclop', 'storm', 'Profesor X', 'Gambit', 'Jean Grey')
+$counter = 0
+
+Write-Host "WHILE LOOP"
+while ($counter -ne 6) {
+    Write-Host "To me" $Xmen[$counter]
+    $Xmen[$counter].Length
+    $counter++;
+}
+
+Write-Host "------------------------------"
+Write-Host " DO WHILE LOOP"
+do {
+    
+} while (
+    <# Condition that stops the loop if it returns false #>
+)
