dac7449944649359d36fceb7469c57c04d1e1acf
Pieter Gleixner <pjgleixner@msn.com>
2025-07-07 16:16:10 -0400

    Update SwitchCases.ps1

--- Learning101/SwitchCases.ps1
+++ Learning101/SwitchCases.ps1
@@ -1,6 +1,9 @@
 $house = "Targaryen"
+#$house = ""
 
 switch ($house) {
-    "Targaryen" {  }
-    Default {}
+    "Targaryen" { Write-Host "Fire and Blood"; break }
+    "Lannister" { Write-Host "A Lannister always pays his debts"; break }
+    "Stark" { Write-Host "Winter is coming" ; break }
+    Default {Write-Host "Game of thrones"}
 }
