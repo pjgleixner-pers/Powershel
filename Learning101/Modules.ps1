bc70fe08feee9123a6d3c3fe190c6f4639ce80c0
Pieter Gleixner <pjgleixner@msn.com>
2025-07-09 00:00:42 -0400

    Update Modules.ps1

--- Learning101/Modules.ps1
+++ Learning101/Modules.ps1
@@ -1,5 +1,5 @@
-Import-Module <# TODO:Module path #> -Force
 
-$Connection=Connect-SQLServer -InstanceName "Servername" -DatabaseName "Logging" -IntergratedSecurity $true
 
 Close-SQLServerConnection -Connection $Connection
+
+Invoke-SQLSelect -Connection $Connection -SelectStatement "SELECT * From Log"
