26e5cb5781e4410583193ec81ead33ce559eb2cf
Pieter Gleixner <pjgleixner@msn.com>
2025-07-09 00:01:35 -0400

    Update Modules.ps1

--- Learning101/Modules.ps1
+++ Learning101/Modules.ps1
@@ -1,4 +1,6 @@
+Import-Module C:\Users\pjgle\Documents\Develop\PowerShell\Powershel\SQLModule\SQLModule.psm1 -Force
 
+$Connection=Connect-SQLServer -InstanceName "PIETERPC" -DatabaseName "Logging" -IntergratedSecurity $true
 
 Close-SQLServerConnection -Connection $Connection
 
